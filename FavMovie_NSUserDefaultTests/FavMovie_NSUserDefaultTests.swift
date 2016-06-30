2e65cebf995f83815cb60b65f80378b8aeab2d58
Younoussa Ousmane <younoussa.ousmane@yahoo.fr>
2016-06-30 15:25:50 +0100

    Initial Commit

---
+++ FavMovie_NSUserDefaultTests/FavMovie_NSUserDefaultTests.swift
@@ -0,0 +1,36 @@
+//
+//  FavMovie_NSUserDefaultTests.swift
+//  FavMovie_NSUserDefaultTests
+//
+//  Created by Younoussa Ousmane Abdou on 6/30/16.
+//  Copyright © 2016 Younoussa Ousmane Abdou. All rights reserved.
+//
+
+import XCTest
+@testable import FavMovie_NSUserDefault
+
+class FavMovie_NSUserDefaultTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
